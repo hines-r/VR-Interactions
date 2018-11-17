7eee226d4445f65d9ce8958ab5233b527a3b9874
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-16 22:28:03 -0700

    Added raycast length for forward vector

--- VR Interactions/Assets/Scripts/PhysicsResolver.cs
+++ VR Interactions/Assets/Scripts/PhysicsResolver.cs
@@ -7,8 +7,11 @@ public class PhysicsResolver : MonoBehaviour
 {
     public bool isGrounded;
     public bool isStuckOnSurface;
-    public float rayLength = 0.5f;
+
+    public float rayDownLength = 0.5f;
+    public float rayForwardLength = 0.5f;
     public float inset = 0.1f;
+
     public LayerMask mask;
 
     private Collider coll;
@@ -21,11 +24,12 @@ public class PhysicsResolver : MonoBehaviour
     void Update()
     {
         Vector3 originPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + .15f);
+
+        // Downward raycast
         Ray rayDown = new Ray(transform.position + coll.bounds.extents.y * Vector3.down + inset * Vector3.up, -transform.up);
         RaycastHit hitInfo;
 
-
-        if (Physics.Raycast(rayDown, out hitInfo, rayLength, mask))
+        if (Physics.Raycast(rayDown, out hitInfo, rayDownLength, mask))
         {
             Debug.Log(hitInfo.collider);
             Debug.DrawLine(rayDown.origin, hitInfo.point, Color.red);
@@ -33,13 +37,14 @@ public class PhysicsResolver : MonoBehaviour
         }
         else
         {
-            Debug.DrawLine(rayDown.origin, rayDown.origin + rayDown.direction * rayLength, Color.green);
+            Debug.DrawLine(rayDown.origin, rayDown.origin + rayDown.direction * rayDownLength, Color.green);
             isGrounded = false;
         }
 
-        Ray rayForward = new Ray(transform.position + coll.bounds.extents.y * Vector3.forward, transform.forward);
+        // Forward raycast
+        Ray rayForward = new Ray(transform.position + coll.bounds.extents.x * Vector3.forward, transform.forward);
 
-        if (Physics.Raycast(rayForward, out hitInfo, rayLength, mask))
+        if (Physics.Raycast(rayForward, out hitInfo, rayForwardLength, mask))
         {
             Debug.Log(hitInfo.collider);
             Debug.DrawLine(rayForward.origin, hitInfo.point, Color.red);
@@ -47,7 +52,7 @@ public class PhysicsResolver : MonoBehaviour
         }
         else
         {
-            Debug.DrawLine(rayForward.origin, rayForward.origin + rayForward.direction * rayLength, Color.green);
+            Debug.DrawLine(rayForward.origin, rayForward.origin + rayForward.direction * rayForwardLength, Color.green);
             isStuckOnSurface = false;
         }
     }
