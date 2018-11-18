f581c3faab866bfa75dcc03b1e88f6612b684287
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-17 17:06:50 -0700

    Body collider now rotates with vr camera on y axis

--- VR Interactions/Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
+++ VR Interactions/Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
@@ -31,6 +31,9 @@ namespace Valve.VR.InteractionSystem
             float distanceFromFloor = Vector3.Dot(head.localPosition, Vector3.up);
             capsuleCollider.height = Mathf.Max(capsuleCollider.radius, distanceFromFloor);
             transform.localPosition = head.localPosition - 0.5f * distanceFromFloor * Vector3.up;
+
+            Vector3 eulerRotation = new Vector3(0, head.transform.eulerAngles.y, 0);
+            transform.rotation = Quaternion.Euler(eulerRotation);
         }
     }
 }
