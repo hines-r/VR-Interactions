ada58ad679661877015c477953c41b1993b1dcf6
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-19 18:04:18 -0700

    Added simple grenade interactable

---
+++ VR Interactions/Assets/Scripts/Grenade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class Grenade : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+    public float minMagnitudeToExplode = 1f;
+
+    private Interactable interactable;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (interactable != null && interactable.attachedToHand != null) return;
+
+        if (collision.impulse.magnitude > minMagnitudeToExplode)
+        {
+            GameObject explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(explosion, 5f);
+
+            Destroy(gameObject); // Destroy grenade itself
+        }
+    }
+}
