f7300cf1ded33cdcaf37fc786b5ebe1677250ca4
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-20 20:25:46 -0700

    Added a grenade spawn table

---
+++ VR Interactions/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject itemToSpawn;
+
+    private GameObject currentItem;
+
+    void Update()
+    {
+        if (currentItem == null)
+        {
+            SpawnItem();
+        }
+    }
+
+    void SpawnItem()
+    {
+        currentItem = Instantiate(itemToSpawn, transform.position, Quaternion.identity);
+        currentItem.transform.parent = gameObject.transform;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == currentItem)
+        {
+            currentItem = null;
+        }
+    }
+}
