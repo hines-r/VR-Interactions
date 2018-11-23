09a8919ce05102f2a4a4bc1d053a1ec2b100171f
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-22 19:20:11 -0700

    Implemented a simple interactable gun example

---
+++ VR Interactions/Assets/Scripts/Gun.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class Gun : MonoBehaviour
+{
+    [SteamVR_DefaultAction("Squeeze")]
+    public SteamVR_Action_Single squeezeAction;
+
+    [SteamVR_DefaultAction("GrabPinch")]
+    public SteamVR_Action_Boolean grabPinchAction;
+
+    public float range = 100f;
+
+    public GameObject firePoint;
+    public GameObject projectile;
+    private float timeToFire;
+
+    private Interactable interactable;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    void Update()
+    {
+        Ray gunRay = new Ray(firePoint.transform.position, firePoint.transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(gunRay, out hitInfo, range))
+        {
+            Debug.DrawLine(gunRay.origin, hitInfo.point, Color.red);
+
+            if (hitInfo.rigidbody != null)
+            {
+
+            }
+        }
+        else
+        {
+            Debug.DrawLine(gunRay.origin, gunRay.origin + gunRay.direction * range, Color.green);
+        }
+
+        if (interactable != null && interactable.attachedToHand != null)
+        {
+            if (grabPinchAction.GetState(SteamVR_Input_Sources.Any) && Time.time >= timeToFire)
+            {
+                timeToFire = Time.time + 1 / projectile.GetComponent<ProjectileMove>().fireRate;
+                Shoot();
+            }
+
+            float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+
+            if (triggerValue >= 1f)
+            {
+                //Shoot();
+            }
+        }            
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(projectile, firePoint.transform.position, transform.rotation);
+        Destroy(bullet, 10f);
+    }
+}
---
+++ VR Interactions/Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+
+    void Update()
+    {
+        transform.position += transform.forward * (speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        Destroy(gameObject);
+    }
+}
