8e473b12039c0c22fc0323b74859955e4bc05e44
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-25 16:55:45 -0700

    Improved object grab script with on change listeners

---
+++ VR Interactions/Assets/Scripts/ObjectGrab.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(FixedJoint))]
+public class ObjectGrab : MonoBehaviour
+{
+    public GameObject objectToGrab;
+    public Hand hand;
+
+    [SteamVR_DefaultAction("GrabPinch")]
+    public SteamVR_Action_Boolean grabPinchAction;
+
+    [SteamVR_DefaultAction("GrabGrip")]
+    public SteamVR_Action_Boolean grabGripAction;
+
+    private FixedJoint fixedJoint;
+
+    void Start()
+    {
+        fixedJoint = GetComponent<FixedJoint>();
+
+        if (grabPinchAction == null)
+        {
+            Debug.LogError("Grab pinch action not assigned!");
+            return;
+        }
+
+        if (grabGripAction == null)
+        {
+            Debug.LogError("Grab grip action not assigned!");
+            return;
+        }
+
+        grabPinchAction.AddOnChangeListener(OnGrabPinchChange, hand.handType);
+        grabGripAction.AddOnChangeListener(OnGrabGripChange, hand.handType);
+    }
+
+    void OnGrabPinchChange(SteamVR_Action_In actionIn)
+    {
+        if (grabPinchAction.GetStateDown(hand.handType))
+        {
+            PickupObj();
+        }
+    }
+
+    void OnGrabGripChange(SteamVR_Action_In actionIn)
+    {
+        if (grabGripAction.GetStateDown(hand.handType))
+        {
+            DropObj();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            objectToGrab = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        objectToGrab = null;
+    }
+
+    void PickupObj()
+    {
+        if (objectToGrab != null)
+        {
+            Rigidbody objectRigidbody = objectToGrab.GetComponent<Rigidbody>();
+
+            if (objectRigidbody != null)
+            {
+                fixedJoint.connectedBody = objectRigidbody;
+            }      
+        }
+        else
+        {
+            fixedJoint.connectedBody = null;
+        }
+    }
+
+    void DropObj()
+    {
+        if (fixedJoint.connectedBody != null)
+        {
+            fixedJoint.connectedBody = null;
+        }
+    }
+}
