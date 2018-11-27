fc166534d88c1964202b9dc7e63e7f48b86360ee
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-26 18:23:32 -0700

    Created simple raycast laser for player hands

---
+++ VR Interactions/Assets/Scripts/Laser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+    public float laserLength = 100f;
+    private LineRenderer lr;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.useWorldSpace = true;
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo))
+        {
+            if (hitInfo.collider != null)
+            {
+                lr.SetPosition(1, hitInfo.point);
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, transform.forward * laserLength);
+        }
+    }
+}
