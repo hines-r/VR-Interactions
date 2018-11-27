01a92c168b7fea4d4984403bb6a4b0043c107b65
hines-r <39021120+hines-r@users.noreply.github.com>
2018-11-27 00:26:07 -0700

    Lasers now produce an impact effect on surfaces hit

--- VR Interactions/Assets/Scripts/Laser.cs
+++ VR Interactions/Assets/Scripts/Laser.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Laser : MonoBehaviour
 {
+    public ParticleSystem impactEffect;
     public float laserLength = 100f;
+
     private LineRenderer lr;
 
     void Start()
@@ -25,11 +27,21 @@ public class Laser : MonoBehaviour
             if (hitInfo.collider != null)
             {
                 lr.SetPosition(1, hitInfo.point);
+                
+                if (!impactEffect.isPlaying)
+                {
+                    impactEffect.Play();
+                }
+
+                Vector3 dir = transform.position - hitInfo.point;
+                impactEffect.transform.position = hitInfo.point;
+                impactEffect.transform.rotation = Quaternion.LookRotation(dir);
             }
         }
         else
         {
             lr.SetPosition(1, transform.forward * laserLength);
+            impactEffect.Stop();
         }
     }
 }
