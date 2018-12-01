e9c08d6e8eecac0559ad6ab1e016317aa1ad4b40
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-01 15:00:26 -0700

    Implemented a way to reset the scene via an interactable cube

---
+++ VR Interactions/Assets/Scripts/ResetManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+public class ResetManager : MonoBehaviour
+{
+    [SteamVR_DefaultAction("Squeeze")]
+    public SteamVR_Action_Single squeezeAction;
+
+    private Hand hoverHand;
+
+    void Update()
+    {
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        if (triggerValue >= 1f && hoverHand != null)
+        {
+            ResetScene();
+        }
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            hoverHand = other.gameObject.GetComponent<Hand>();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            if (hoverHand != null)
+            {
+                hoverHand = null;
+            }
+        }
+    }
+}
