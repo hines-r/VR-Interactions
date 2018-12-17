c346f959a78e4d51c62a375bc41ff3811940d4ec
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-17 13:56:17 -0700

    Implemented new jump pad

---
+++ VR Interactions/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 5f;
+
+    void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+        }
+    }
+}
